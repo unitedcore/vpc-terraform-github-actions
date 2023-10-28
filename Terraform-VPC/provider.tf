6833441d9322cc211975868484ba014988eecee7
unitedcore <145376907+unitedcore@users.noreply.github.com>
2023-10-27 20:17:16 -0400

    Update provider.tf

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "my-terraform-remote-backend-s3"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+  // backend "s3" {
+    // bucket = "my-terraform-remote-backend-s3"
+    // key    = "dev/terraform.tfstate"
+    // region = "us-east-1"
+ //  }
+// }
 
 provider "aws" {
   region = "us-east-1"
